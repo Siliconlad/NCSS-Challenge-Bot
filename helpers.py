f799f0f2c3ad06a982c57710ef5e2fa44f82225f
Angus Stewart <38025909+Siliconlad@users.noreply.github.com>
2018-08-18 18:59:19 +1200

    Added a docstring to the function

--- helpers.py
+++ helpers.py
@@ -1,4 +1,22 @@
 def is_higher(card1, card2):
+    '''
+    The function compares two cards to see which is ranked higher.
+
+    The function compares two valid cards and returns true if card1 is ranked higher and false if it does not. Does not assume that the cards must be from the same deck. If two equivalent cards are passed then the function will return False because a card is not ranked higher than itself.
+
+    Keywords arguements:
+    card1 -- first card of type string (required)
+    card2 -- second card of type string (required)
+
+    Return type:
+    True -- when card1 is ranked higher than card2
+    False -- when card1 is ranked lower than card2
+
+    Function assumptions:
+    -- Valid card format e.g. '3D'
+    -- Valid card e.g. NOT '9Y'
+    '''
+
     suit_score = {"D": 0, "C": 1, "H": 2, "S": 3}
     rank_score = {"3":0, "4":1, "5":2, "6":3, "7":4, "8":5, "9":6, "0":7, "J":8, "Q":9, "K":10, "A":11, "2":12}
 
@@ -15,4 +33,4 @@ def is_higher(card1, card2):
         card1_suit_rank = rank_score[card1[0]]
         card2_suit_rank = rank_score[card2[0]]
 
-        return False if card1_suit_rank < card2_suit_rank else True
+        return True if card1_suit_rank > card2_suit_rank else False

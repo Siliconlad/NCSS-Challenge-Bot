9c407f4103b8cf6309f6f795ceb89f80d0cb3119
Angus Stewart <38025909+Siliconlad@users.noreply.github.com>
2018-08-21 14:30:44 +1200

    Created test function for function comparing two single cards

--- helpers_test.py
+++ helpers_test.py
@@ -1,7 +1,13 @@
 import unittest
+from helpers import is_higher
+from helpers import sort
 
 class Test(unittest.TestCase):
-
+    def test_is_higher(self):
+        self.assertEqual(False, is_higher('3D', '3C'))
+        self.assertEqual(True, is_higher('JS', 'JD'))
+        self.assertEqual(True, is_higher('2S', '3D'))
+        self.assertEqual(False, is_higher('3D', '3D'))
 
 if __name__ == '__main__':
     unittest.main()

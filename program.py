76c6e4fa6a1a2ad1fcf181c09d5a7a9ca4827e8b
Angus Stewart <38025909+Siliconlad@users.noreply.github.com>
2018-08-21 20:32:10 +1200

    Basic bot that plays the lowest card possible

--- program.py
+++ program.py
@@ -1,5 +1,17 @@
 from helpers import *
 
 def play(hand, is_start_of_round, play_to_beat, round_history, player_no, hand_sizes, scores, round_no):
-    sorted_hand = sorted(hand)
+    # Sort the hand because all functions assume the hand is sorted
+    sorted_hand = sort(hand)
+
+    # If start of round then must play 3D
+    if is_start_of_round:
+        return ['3D']
+
+    playable_cards = playable(sorted_hand, play_to_beat)
+    # Play lowest card
+    if len(playable_cards) != 0:
+        return [playable_cards[0]]
+    else:
+        return []
 
--- program_test.py
+++ program_test.py
@@ -1,7 +1,14 @@
 import unittest
+from program import play
 
 class Test(unittest.TestCase):
     def test_play(self):
+        hand = ['7H', '8D', '8S', '9S', '0S', 'JD', 'JH', 'JS', 'KH']
+
+        self.assertEqual(['7H'], play(hand, False, [], [[]], 0, [9, 9, 10, 10], [0, 0, 0, 0], 0))
+        self.assertEqual(['9S'], play(hand, False, ['9D'], [[]], 0, [9, 9, 10, 10], [0, 0, 0, 0], 0))
+
+
 
 if __name__ == '__main__':
     unittest.main()

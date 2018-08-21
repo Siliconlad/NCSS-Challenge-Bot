b6e2d1983556e676452486d4b4647bd3899c126e
Angus Stewart <38025909+Siliconlad@users.noreply.github.com>
2018-08-21 20:50:35 +1200

    Test cases for playing highest card when a player has one card left

--- program_test.py
+++ program_test.py
@@ -5,9 +5,13 @@ class Test(unittest.TestCase):
     def test_play(self):
         hand = ['7H', '8D', '8S', '9S', '0S', 'JD', 'JH', 'JS', 'KH']
 
+        # Basic play lowest possible card tests
         self.assertEqual(['7H'], play(hand, False, [], [[]], 0, [9, 9, 10, 10], [0, 0, 0, 0], 0))
         self.assertEqual(['9S'], play(hand, False, ['9D'], [[]], 0, [9, 9, 10, 10], [0, 0, 0, 0], 0))
 
+        # Tests for when a player has one card left
+        self.assertEqual(['KH'], play(hand, False, ['9D'], [[]], 0, [9, 9, 10, 1], [0, 0, 0, 0], 0))
+        self.assertEqual(['KH'], play(['KH'], False, ['9D'], [[]], 0, [1, 9, 10, 10], [0, 0, 0, 0], 0))
 
 
 if __name__ == '__main__':

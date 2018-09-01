c294f1e35007612f1daa9b6339b3c4dbfccd40a8
Angus Stewart <38025909+Siliconlad@users.noreply.github.com>
2018-09-01 21:24:06 +1200

    Cleaned up code

--- program.py
+++ program.py
@@ -1,12 +1,12 @@
 from helpers import *
 
 def play_singles(sorted_hand, play_to_beat, round_history, hand_sizes):
-    size_of_hand = len(sorted_hand)
-
     highest_cards = highest(sorted_hand, round_history)
     size_of_highest = len(highest_cards)
+    
     if size_of_highest != 0:
         # If a player can win the round by playing all their highest cards 
+        size_of_hand = len(sorted_hand)
         if size_of_highest >= size_of_hand-1:
             playable_highest = playable(highest_cards, play_to_beat)
             if len(playable_highest) != 0:
@@ -36,7 +36,6 @@ def play_pairs(sorted_hand, play_to_beat):
     triples = all_triples(sorted_hand)
     # Play the lowest card not in triples
     for pair in playable_cards:
-        
         if not_in_triple(pair, triples):
             return pair
     return []
@@ -56,11 +55,10 @@ def play(hand, is_start_of_round, play_to_beat, round_history, player_no, hand_s
     # Sort the hand because all functions assume the hand is sorted
     sorted_hand = sort(hand)
 
-    triples = all_triples(sorted_hand)
-    pairs = all_pairs(sorted_hand)
     if is_start_of_round:
-        # Because pairs and triples are sorted first item must be the lowest
-        # Hence for loop does not cost anything
+        triples = all_triples(sorted_hand)
+        pairs = all_pairs(sorted_hand)
+        # Because pairs and triples are sorted first item must be the lowest hence for loop does not cost anything
         for triple in triples:
             if '3D' in triple[0]:
                 return triple
@@ -72,30 +70,31 @@ def play(hand, is_start_of_round, play_to_beat, round_history, player_no, hand_s
         return ['3D']
 
     playable_cards = playable(sorted_hand, play_to_beat)
-    size_of_playable = len(playable_cards)
     # Allows rest of program to assume there is at least one card to play
-    if size_of_playable == 0:
+    if len(playable_cards) == 0:
         return []
 
     size_of_play_to_beat = len(play_to_beat)
-    # If we are starting a trick
+    # Starting a trick
     if size_of_play_to_beat == 0:
         output = play_triples(sorted_hand, play_to_beat)
 
-        if output == []:
+        if not output:
             output = play_pairs(sorted_hand, play_to_beat)
 
-        if output == []:
+        if not output:
             output =  play_singles(sorted_hand, play_to_beat, round_history, hand_sizes)
 
         return output
     # Rules for single cards
     elif size_of_play_to_beat == 1:
         return play_singles(sorted_hand, play_to_beat, round_history, hand_sizes)
-    # Rules for playing doubles
+    # Playing doubles
     elif size_of_play_to_beat == 2:
         return play_pairs(sorted_hand, play_to_beat)
+    # Playing triples
     elif size_of_play_to_beat == 3:
         return play_triples(sorted_hand, play_to_beat)
+    # Random error
     else:
         return []
